b3b8c917e942f589c171c0ad3ef2b9ff71dc711c
Pieter Koornhof <koornhof.pieter@gmail.com>
2020-05-20 09:14:50 +0400

    Chore: add for field and inputs

--- src/b/components/form.cljs
+++ src/b/components/form.cljs
@@ -30,7 +30,11 @@
    [{:type ::tags
      :id   :tags
      :label "Test Tags"
-     :description "mini description"}]})
+     :description "mini description"}
+    {:type ::text
+     :id   :title
+     :label "Test Title"
+     :description "mini description 2"}]})
 
 
 (defmulti ^:private render-field (fn [answer f v]
@@ -51,12 +55,18 @@
                     [::set-form-field data-path (:id field) transform default a])))]
     [:div
      (when debug?
-       [:p (str data)])
+       [:p.has-text-centered (str data)])
      (->> (:fields form-config)
           (map-indexed
            (fn [i f]
-             [:div {:key i}
-              (render-field (answer f) f (get data (:id f)))])))]))
+             [:div.field.is-horizontal {:key i}
+              [:div.field-label.is-normal
+               [:label.label (:label f)]]
+              [:div.field-body
+               [:div.field
+                (render-field (answer f) f (get data (:id f)))
+                (when-let [d (:description f)]
+                  [:p.help d])]]])))]))
 
 
 (defmethod render-field ::tags
@@ -67,3 +77,11 @@
       :select #(answer % :transform conj :default #{})
       :de-select #(answer % :transform disj :default #{})
       :selected-tag-ids (or v #{})]]))
+
+
+(defmethod render-field ::text
+  [answer f v]
+  [:div.control
+   [:input.input {:type "text" :value (or v "")
+                  :on-change #(answer (.. % -target -value) :default "")}]
+   ])
